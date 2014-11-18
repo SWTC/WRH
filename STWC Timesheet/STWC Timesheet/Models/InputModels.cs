ddfa75c7263fc7ac8108826072995458eb76287f
shriramhrs <shriram_hrs@yahoo.com>
2014-11-18 20:12:59 +0530

    UI Changes
    UI Changes

---
+++ STWC Timesheet/STWC Timesheet/Models/InputModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace STWC_Timesheet.Models
+{
+    public class InputModel
+    {
+        [Required]
+        [Display(Name = "Crew Member")]
+        public string CrewMember { get; set; }
+
+        [Required]
+        [Display(Name = "Position")]
+        public string Position { get; set; }
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Comments")]
+        public string Comments { get; set; }
+
+        [Display(Name = "Service Terms")]
+        public string ServiceTerms { get; set; }
+
+        [Display(Name = "Non Conformities")]
+        public string NonConformities { get; set; }
+    }
+
+    
+}
